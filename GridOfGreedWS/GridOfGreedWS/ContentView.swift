bb2781e01070adbedb72f0747d7822887dd372bc
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-09-08 22:13:48 +0800

    Create 2D arrays and square toggeles 25x25

--- GridOfGreedWS/GridOfGreedWS/ContentView.swift
+++ GridOfGreedWS/GridOfGreedWS/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Create a 2D array to track which squares are filled
+    @State private var isFilled: [Bool] = Array(repeating: false, count: 25)
+    
+    // Define a grid with 5 squares
+    let square = Array(repeating: GridItem(.fixed(20)), count: 5)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        LazyVGrid(columns: square) {
+            // Loop through 25 items (5x5 grid)
+            ForEach(0..<25, id: \.self) { index in
+                Image(systemName: isFilled[index] ? "square.fill" : "square")
+                    .imageScale(.large)
+                    .foregroundStyle(Color.secondary)
+                    .onTapGesture {
+                        isFilled[index].toggle()
+                    }
+                    .frame(width: 20, height: 20)
+            }
+        }.padding()
+      
+        Text("Greedy folks...")
+            .padding()
+            .foregroundStyle(Color.cyan)
     }
 }
 
