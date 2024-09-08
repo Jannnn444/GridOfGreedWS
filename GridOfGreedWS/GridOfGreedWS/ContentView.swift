43d723e98b93b48061fa47243d031653f9275532
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-09-08 22:44:04 +0800

    Changed the size of squares

--- GridOfGreedWS/GridOfGreedWS/ContentView.swift
+++ GridOfGreedWS/GridOfGreedWS/ContentView.swift
@@ -10,30 +10,35 @@ import SwiftUI
 struct ContentView: View {
     
     // Create a 2D array to track which squares are filled
-    @State private var isFilled: [Bool] = Array(repeating: false, count: 25)
+    @State private var isFilled: [Bool] = Array(repeating: false, count: 50)
     
     // Define a grid with 5 squares
-    let square = Array(repeating: GridItem(.fixed(20)), count: 5)
+    let square = Array(repeating: GridItem(.fixed(50), spacing: 0), count: 5)
     
     var body: some View {
-        LazyVGrid(columns: square) {
-            // Loop through 25 items (5x5 grid)
-            ForEach(0..<25, id: \.self) { index in
-                Image(systemName: isFilled[index] ? "square.fill" : "square")
-                    .imageScale(.large)
-                    .foregroundStyle(Color.cyan)
-                    .onTapGesture {
-                        isFilled[index].toggle()
-                    }
-                    .frame(width: 20, height: 20)
+        ScrollView {
+            LazyVGrid(columns: square, spacing: 0) { // Remove spacing between columns
+                // Loop through 25 items (5x5 grid)
+                ForEach(0..<50, id: \.self) { index in
+                    Image(systemName: isFilled[index] ? "square.fill" : "square")
+                        .resizable()
+                        .imageScale(.large)
+                        .foregroundStyle(Color.cyan)
+                        .onTapGesture {
+                            isFilled[index].toggle()
+                        }
+                        .frame(width: 50, height: 50)
+                }
             }
-        }.padding()
+            .padding(.vertical, 30)
+        }
       
         Text("Greedy folks...")
             .bold()
             .fontDesign(.serif)
             .padding()
             .foregroundStyle(Color.cyan)
+        
     }
 }
 
