941df20a70f2e6f8e449c3fa65567e1a020c85e0
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-09-08 19:36:15 +0800

    Create xcode SwiftUI project.

---
+++ GridOfGreedWS/GridOfGreedWS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GridOfGreedWS
+//
+//  Created by yucian huang on 2024/9/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GridOfGreedWS/GridOfGreedWS/GridOfGreedWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridOfGreedWSApp.swift
+//  GridOfGreedWS
+//
+//  Created by yucian huang on 2024/9/8.
+//
+
+import SwiftUI
+
+@main
+struct GridOfGreedWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
